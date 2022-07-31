38cb340c7efc41c6ae1603e068bfe1ae39153cd9
Narayan soni <77617189+narayan954@users.noreply.github.com>
2022-07-31 23:36:52 +0530

    Create Farthest number.py

---
+++ gfg/Farthest number.py
@@ -0,0 +1,44 @@
+from typing import List
+
+
+class Solution:
+    def farNumber(self, N : int, A : List[int]) -> List[int]:
+        result = [-1] * len(A)
+        rt_index = -1
+        for val, ind in sorted((val, ind) for ind, val in enumerate(A)):
+            result[ind] = rt_index if rt_index > ind else -1
+            rt_index = max(rt_index, ind)
+        return result
+        
+
+
+#{ 
+#  Driver Code Starts
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        N = int(input())
+        
+        
+        A=IntArray().Input(N)
+        
+        obj = Solution()
+        res = obj.farNumber(N, A)
+        
+        IntArray().Print(res)
+        
+
+# } Driver Code Ends
