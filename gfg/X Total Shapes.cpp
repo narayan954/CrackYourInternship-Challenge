a83a93722b19ef6e1f7d3bbb8b409891cb74a12e
Narayan soni <77617189+narayan954@users.noreply.github.com>
2022-08-07 23:37:31 +0530

    Update X Total Shapes.cpp

--- gfg/X Total Shapes.cpp
+++ gfg/X Total Shapes.cpp
@@ -8,50 +8,36 @@ using namespace std;
 class Solution
 {
     public:
+    void dfs(vector<vector<char>>& grid, int i, int j){
+        int n = grid.size();
+        int m = grid[0].size();
+        if(i<0||j<0||i>=n||j>=m) return;
+        if(grid[i][j] != 'X') return;
+        grid[i][j] = '#';
+        dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
+        dfs(grid,i-1,j);
+        dfs(grid,i+1,j);
+    }
+    
     //Function to find the number of 'X' total shapes.
-    int vis[100][100]={{0}};
-    int dx[4]={-1,1,0,0};
-    int dy[4]={0,0,-1,1};
-    int n1,m1;
-    
-    bool val(int r,int c){
-        if(r>=n1 || r<0 || c>=m1 || c<0) return false;
-        else return true;
-    }
-    
-    void dfs(int r,int c){
-        vis[r][c]=1;
-        for(int i=0;i<4;i++){
-            int nr=r+dx[i];
-            int nc=c+dy[i];
-            if(vis[nr][nc]==0 && val(nr,nc)==true) dfs(nr,nc);
-        }
-    }
-    
-    int xShape(vector<vector<char>>& grid) {
+    int xShape(vector<vector<char>>& grid) 
+    {
         // Code here
-        int count=0;
-        int r=grid.size();
-        int c=grid[0].size();
-        n1=r;
-        m1=c;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(grid[i][j]!='X') vis[i][j]=1;
-            }
-        }
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(!vis[i][j]){
-                    dfs(i,j);
+        int n = grid.size();
+        int m = grid[0].size();
+        int count = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == 'X'){
                     count++;
+                    dfs(grid,i,j);
                 }
             }
         }
         return count;
     }
 };
-
 
 //{ Driver Code Starts.
 int main(){
