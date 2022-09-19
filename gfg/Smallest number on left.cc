1181ed1492d22abc12aa3bdac47badc200e18982
Narayan soni <77617189+narayan954@users.noreply.github.com>
2022-09-19 10:32:14 +0530

    Create Smallest number on left.cc

---
+++ gfg/Smallest number on left.cc
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<int> leftSmaller(int n, int a[]){
+        // code here
+        vector<int> ans(n,-1);
+        for(int i=n-1;i>-1;i--){
+            for(int x=i-1;x>-1;x--){
+                if(a[x]<a[i]){
+                    ans[i]=a[x];
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i = 0;i < n;i++)
+            cin>>a[i];
+        
+        Solution ob;
+        vector<int> ans = ob.leftSmaller(n, a);
+        for(int i = 0;i < n;i++)
+            cout<<ans[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
