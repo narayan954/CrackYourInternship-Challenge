d4a15068509f598dc6a1aba96bb73df24715153b
Narayan soni <77617189+narayan954@users.noreply.github.com>
2022-09-23 22:37:19 +0530

    Create Geek and Number String.py

---
+++ gfg/Geek and Number String.py
@@ -0,0 +1,27 @@
+#User function Template for python3
+
+class Solution:
+    def minLength(self, s, n): 
+        ls = [9,2,1,4,3,6,5,8,7,0]
+        st = []
+        
+        for i in range(n):
+            if st and ls[st[-1]]==int(s[i]):
+                st.pop()
+            else:
+                st.append(int(s[i]))
+                
+        return len(st)
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        s = input()
+        ob = Solution()
+        print(ob.minLength(s,n))
+# } Driver Code Ends
