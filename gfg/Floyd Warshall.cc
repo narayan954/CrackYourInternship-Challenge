3e4038018ea828d77142a485b46f65519ebcdd11
Narayan soni <narayansoni854@gmail.com>
2022-11-08 02:28:16 +0530

    Create Floyd Warshall.cc

---
+++ gfg/Floyd Warshall.cc
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution {
+  public:
+	void shortest_distance(vector<vector<int>>&matrix){
+	    int n = matrix.size();
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<n;j++){
+	            for(int k=0;k<n;k++){
+	                if(j==i || k==i || matrix[j][i]==-1 || matrix[i][k]==-1) continue;
+	                int curr = matrix[j][i] + matrix[i][k];
+	                if(matrix[j][k]==-1) matrix[j][k] = curr;
+	                else matrix[j][k] = min(curr, matrix[j][k]);
+	            }
+	        }
+	    }
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>>matrix(n, vector<int>(n, -1));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				cin >> matrix[i][j];
+			}
+		}
+		Solution obj;
+		obj.shortest_distance(matrix);
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				cout << matrix[i][j] << " ";
+			}
+			cout << "\n";
+		}
+	}
+	return 0;
+}
+// } Driver Code Ends
