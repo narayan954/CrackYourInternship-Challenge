fc719e469fd2c4e50a9490c24ac7efee457d1010
Narayan soni <narayansoni854@gmail.com>
2022-11-23 22:35:07 +0530

    Create Maximum Sum LCM.cc

---
+++ gfg/Maximum Sum LCM.cc
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution {
+  public:
+    long long int maxSumLCM(int n) {
+        long long ans = 0;
+        for(int i=1;i*i<=n;i++){
+            if(!(n%i)){
+                ans += i;
+                if(i!=(n/i)) ans += n/i;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int  n;
+        cin >> n ;
+        Solution ob;
+        cout<<ob.maxSumLCM(n)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
